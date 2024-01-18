9722eeac5d0a6c53a75a24a376578f9bc6f3905e
AJ Cooper <acooper@lcs.on.ca>
2024-01-18 15:00:01 -0500

    Made a scroll view as well as a navigation view to present the selection of what card you'd like to see. I made a file to show what person is being presented when you click on the navigation view.

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -8,17 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var names = ["Scott", "Mark", "Chris", "Sean", "Rod", "Meng", "Natasha","Chase", "Evans", "Paul", "Durtschi", "Max", "Fart"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            ForEach(names, id: \.self) { name in
+                NavigationView(content: {
+                    NavigationLink(
+                        destination: Person()
+                    ) {
+                        HStack {
+                            Text(name)
+                                .foregroundStyle(.primary)
+                            Image(systemName: "checkmark.seal.fill")
+                                .foregroundStyle(.green)
+                            Spacer()
+                            
+                            
+                        }
+                    }
+                })
+                
+                .padding()
+                .background(Color.white.shadow(.drop(radius: 1, y: 1)),
+                            in: RoundedRectangle(cornerRadius: 8))
+                .padding(.bottom, 4)
+            }
+            .padding(.horizontal)
         }
-        .padding()
+        .font(.title)
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
---
+++ TradingCards/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  TradingCards
+//
+//  Created by  Adam-James  Cooper on 2024-01-18.
+//
+
+import SwiftUI
+
+struct Person: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Person()
+}
