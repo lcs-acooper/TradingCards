5d65ec9571dc76872ce359161b92faebbdd471a0
AJ Cooper <acooper@lcs.on.ca>
2024-01-21 15:36:13 -0500

    Added card info

--- TradingCards/CardInfo.swift
+++ TradingCards/CardInfo.swift
@@ -9,7 +9,55 @@ import SwiftUI
 
 struct CardInfo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       ZStack {
+           // rectangle background blue
+           Rectangle()
+               .foregroundColor(.blue)
+               .ignoresSafeArea()
+           
+           // rectangle blue
+           VStack {
+               ZStack {
+                   Rectangle()
+                       .foregroundColor(.red)
+                       .cornerRadius(20)
+                   .padding(20)
+                   
+                   //rectangle white
+                   Rectangle()
+                       .foregroundColor(.green)
+                       .cornerRadius(20)
+                       .padding(40)
+                   
+                   Image("Example")
+                       .resizable()
+                       .scaledToFit()
+                       .cornerRadius(20)
+                       .padding(60)
+                      
+                       
+               }
+               ZStack {
+                   VStack {
+                       Text("Card Stats")
+                           .foregroundStyle(.white)
+                           .font(.largeTitle)
+                       Spacer()
+                   }
+                   
+                   Text("Chacteristic")
+                   
+               }
+               Spacer()
+               
+              // text
+               
+          
+              
+           }
+       }
+       .ignoresSafeArea()
+
     }
 }
 
